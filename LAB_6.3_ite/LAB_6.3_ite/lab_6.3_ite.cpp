dc44d0856cfb97d110a1d1380111d536b1522b09
harD1kkkk <137618635+harD1kkkk@users.noreply.github.com>
2024-11-18 15:08:52 +0200

    Update lab_6.3_ite.cpp

--- LAB_6.3_ite/LAB_6.3_ite/lab_6.3_ite.cpp
+++ LAB_6.3_ite/LAB_6.3_ite/lab_6.3_ite.cpp
@@ -1,22 +1,6 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
-
-int CountEvenElements(int* a, const int size); 
-template <typename T>
-int CountEvenElements(T* a, const int size); 
-
-int main() {
-    const int n = 10;
-    int a[n] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-
-    int count1 = CountEvenElements(a, n);
-    cout << "CountEvenElements(a, n) = " << count1 << endl;
-
-    int count2 = CountEvenElements<int>(a, n);
-    cout << "CountEvenElements<int>(a, n) = " << count2 << endl;
-
-    return 0;
-}
 
 int CountEvenElements(int* a, const int size) {
     int count = 0;
@@ -37,4 +21,61 @@ int CountEvenElements(T* a, const int size) {
         }
     }
     return count;
+}
+
+void CreateArrayWithRandomElements(int* a, int numberOfElements, int start, int end) {
+    for (int i = 0; i < numberOfElements; i++) {
+        a[i] = start + rand() % (end - start + 1);
+    }
+}
+
+template <typename T>
+void CreateArrayWithRandomElements(T* a, int numberOfElements, int start, int end) {
+    for (int i = 0; i < numberOfElements; i++) {
+        a[i] = start + rand() % (end - start + 1);
+    }
+}
+
+void PrintArray(int* a, int size) {
+    for (int i = 0; i < size; i++) {
+        cout << "a[" << setw(2) << i << " ] = " << setw(4) << a[i] << endl;
+    }
+    cout << endl;
+}
+
+template <typename T>
+void PrintArray(T* a, int size) {
+    for (int i = 0; i < size; i++) {
+        cout << "a[" << setw(2) << i << " ] = " << setw(4) << a[i] << endl;
+    }
+    cout << endl;
+}
+
+int main() {
+    srand((unsigned)time(NULL));
+
+    const int size = 10;
+    int a[size];
+
+    cout << "Create Array" << endl;
+    CreateArrayWithRandomElements(a, size, -20, 34);
+
+    cout << "Print" << endl;
+    PrintArray(a, size);
+
+    int count1 = CountEvenElements(a, size);
+    cout << "CountEvenElements (function) = " << count1 << endl;
+
+    cout << endl;
+
+    cout << "Create Array(template)" << endl;
+    CreateArrayWithRandomElements<int>(a, size, -20, 34);
+
+    cout << "Print(template)" << endl;
+    PrintArray<int>(a, size);
+
+    int count2 = CountEvenElements<int>(a, size);
+    cout << "CountEvenElements (template) = " << count2 << endl;
+
+    return 0;
 }
